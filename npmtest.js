23ef7cbedca2ab25ddf078e474ba07d72a1ab5da
Chris Matthieu <chris@matthieu.us>
2013-10-08 14:49:24 -0700

    added npm client example

---
+++ npmtest.js
@@ -0,0 +1,23 @@
+var skynet = require('skynet');
+// var net = skynet.connect({"host":"localhost", "port": 3000, "uuid": "ad698900-2546-11e3-87fb-c560cb0ca47b", "token": "zh4p7as90pt1q0k98fzvwmc9rmjkyb9"});
+
+var conn = skynet.createConnection({
+  "host":"localhost",
+  "port": 3000,
+  "uuid": "ad698900-2546-11e3-87fb-c560cb0ca47b",
+  "token": "zh4p7as90pt1q0k98fzvwmc9rmjkyb9"
+});
+
+
+conn.on('message', function(data){
+  console.log('status received');
+  console.log(data);
+});
+
+conn.send({
+  "devices": "all",
+  "message": {
+    "contra":"is cool"
+  }
+});
+
