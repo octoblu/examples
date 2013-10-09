357c3e79ae20fe9274427d83aad43413e2f2ed42
Chris Matthieu <chris@matthieu.us>
2013-10-09 10:40:22 -0700

    updated npm example with new responses via onMessage

--- npmtest.js
+++ npmtest.js
@@ -17,6 +17,14 @@ conn.send({
 conn.on('message', function(data){
   console.log('status received');
   console.log(data);
+
+  if (data.request == "register"){
+    conn.unregister({
+      "uuid": data.uuid, 
+      "token": data.token
+    });
+  }
+
 });
 
 // Event triggered when device loses connection to skynet
@@ -29,27 +37,12 @@ conn.register({
   "token": "zh4p7as90pt1q0k98fzvwmc9rmjkyb9", 
   "type": "drone"
 });
-conn.on('register', function(data){
-  console.log('registration received');
-  console.log(data);
-  
-  // UnRegister a device
-  conn.unregister({
-    "uuid": data.uuid, 
-    "token": data.token
-  });
-
-});
 
 // UnRegister a device
 // conn.unregister({
 //   "uuid": "zh4p7as90pt1q0k98fzvwmc9rmjkyb9", 
 //   "token": "zh4p7as90pt1q0k98fzvwmc9rmjkyb9"
 // });
-conn.on('unregister', function(data){
-  console.log('unregistration received');
-  console.log(data);
-});
 
 
 // Update device
@@ -58,31 +51,15 @@ conn.update({
   "token": "zh4p7as90pt1q0k98fzvwmc9rmjkyb9", 
   "armed":true
 });
-conn.on('update', function(data){
-  console.log('update received');
-  console.log(data);
-});
 
 // WhoAmI?
 conn.whoami({});
-conn.on('whoami', function(data){
-  console.log('whoami received');
-  console.log(data);
-});
 
 // Receive an array of device UUIDs based on user defined search criteria
 conn.devices({
   "type":"drone"
 });
-conn.on('devices', function(data){
-  console.log('devices received');
-  console.log(data);
-});
 
 // Skynet status
 conn.status({});
-conn.on('status', function(data){
-  console.log('status received');
-  console.log(data);
-});
 
