f3666241ba006e393a92aa27f5d0f573e224e060
Chris Matthieu <chris@matthieu.us>
2013-10-09 16:01:50 -0700

    updated websocket api to behave like an rpc call

--- npmtest.js
+++ npmtest.js
@@ -36,12 +36,22 @@ conn.on('disconnect', function(data){
 conn.register({
   "token": "zh4p7as90pt1q0k98fzvwmc9rmjkyb9", 
   "type": "drone"
+}, function (data) {
+  console.log(data); 
+  conn.unregister({
+    "uuid": data.uuid, 
+    "token": data.token
+  }, function (data) {
+    console.log(data); 
+  });  
 });
 
 // UnRegister a device
 // conn.unregister({
 //   "uuid": "zh4p7as90pt1q0k98fzvwmc9rmjkyb9", 
 //   "token": "zh4p7as90pt1q0k98fzvwmc9rmjkyb9"
+// }, function (data) {
+//   console.log(data); 
 // });
 
 
@@ -50,16 +60,24 @@ conn.update({
   "uuid":"ad698900-2546-11e3-87fb-c560cb0ca47b", 
   "token": "zh4p7as90pt1q0k98fzvwmc9rmjkyb9", 
   "armed":true
+}, function (data) {
+  console.log(data); 
 });
 
 // WhoAmI?
-conn.whoami({});
+conn.whoami({"uuid":"ad698900-2546-11e3-87fb-c560cb0ca47b"}, function (data) {
+  console.log(data); 
+});
 
 // Receive an array of device UUIDs based on user defined search criteria
 conn.devices({
   "type":"drone"
+}, function (data) {
+  console.log(data); 
 });
 
 // Skynet status
-conn.status({});
+conn.status(function (data) {
+  console.log(data); 
+});
 
