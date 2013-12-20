f4d6eeb58a81bd78b6bc5b2007ac6747a04c4df8
Chris Matthieu <chris@matthieu.us>
2013-12-19 22:53:50 -0700

    added lcd example

---
+++ lcd.js
@@ -0,0 +1,39 @@
+var five = require("johnny-five"),
+    board, lcd;
+ 
+board = new five.Board();
+var skynet = require('skynet');
+
+var conn = skynet.createConnection({
+  // "host":"localhost",
+  // "port": 3000,
+  "host":"http://skynet.im",
+  "port": 80,
+  "uuid": "0d3a53a0-2a0b-11e3-b09c-ff4de847b2cc",
+  "token": "qirqglm6yb1vpldixflopnux4phtcsor"
+});
+
+conn.on('ready', function(data){
+
+  board.on("ready", function() {
+    var back = new five.Pin(10);
+    lcd = new five.LCD({
+      pins: [ 8, 9, 4, 5, 6, 7 ],
+    });
+   
+    lcd.on("ready", function() {
+      conn.on('message', function(data){
+
+        lcd.useChar('heart');
+        lcd.clear().print(data.text);
+        // lcd.cursor(1, 0);
+        // lcd.print("We :heart: johnny-five");
+
+      });
+    });
+   
+   
+  });
+
+
+});
