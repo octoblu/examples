281addcb02e883915cd7cc58f899b532900f4c08
Chris Matthieu <chris@matthieu.us>
2014-02-12 09:48:57 -0700

    removed node modules lib

--- cylon.js
+++ cylon.js
@@ -1,43 +1,41 @@
 var skynet = require('skynet');
 var Cylon = require("cylon");
 
-// Initialize the robot
-var robot = Cylon.robot({
-  // Change the port to the correct port for your Arduino.
-  connection: { name: 'arduino', adaptor: 'firmata', port: '/dev/tty.usbmodem1451' },
-  device: { name: 'led', driver: 'led', pin: 13 },
-
-  work: function(my) {
-    // we do our thing here
-    // every((1).second(), function() { my.led.toggle(); });
-
-    var conn = skynet.createConnection({
-      "uuid": "742401f1-87a4-11e3-834d-670dadc0ddbf",
-      "token": "xjq9h3yzhemf5hfrme8y08fh0sm50zfr",
-      "protocol": "mqtt"
-    });
-
-    conn.on('ready', function(data){
-
-      console.log('Connected to Skynet...');
-
-      conn.on('message', function(channel, data){
-        console.log(data);
-        data = JSON.parse(data);
-        if(data.red == 'on'){
-          console.log("red on request received from skynet");
-          my.led.turnOn();
-
-        } else if(data.red == 'off'){
-          console.log("red off request received from skynet");
-          my.led.turnOff();
-        }     
-      });
-
-    });
-
-
-  }
+var conn = skynet.createConnection({
+  "uuid": "742401f1-87a4-11e3-834d-670dadc0ddbf",
+  "token": "xjq9h3yzhemf5hfrme8y08fh0sm50zfr",
+  "protocol": "mqtt"
 });
-robot.start();
 
+conn.on('ready', function(data){
+
+  console.log('Connected to Skynet...');
+
+  // Initialize the robot
+  var robot = Cylon.robot({
+    // Change the port to the correct port for your Arduino.
+    connection: { name: 'arduino', adaptor: 'firmata', port: '/dev/tty.usbmodem1451' },
+    device: { name: 'led', driver: 'led', pin: 13 },
+
+    work: function(my) {
+      // we do our thing here
+      // every((1).second(), function() { my.led.toggle(); });
+
+        conn.on('message', function(channel, data){
+          console.log(data);
+          data = JSON.parse(data);
+          if(data.red == 'on'){
+            console.log("red on request received from skynet");
+            my.led.turnOn();
+
+          } else if(data.red == 'off'){
+            console.log("red off request received from skynet");
+            my.led.turnOff();
+          }     
+        });
+
+    }
+  });
+  robot.start();
+
+});
