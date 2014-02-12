1f7db917df97970c1a8801f05a18dbcd11ae2550
Chris Matthieu <chris@matthieu.us>
2014-02-12 09:39:52 -0700

    refactored cylon

--- cylon.js
+++ cylon.js
@@ -1,3 +1,4 @@
+var skynet = require('skynet');
 var Cylon = require("cylon");
 
 // Initialize the robot
@@ -8,34 +9,35 @@ var robot = Cylon.robot({
 
   work: function(my) {
     // we do our thing here
-    every((1).second(), function() { my.led.toggle(); });
+    // every((1).second(), function() { my.led.toggle(); });
+
+    var conn = skynet.createConnection({
+      "uuid": "742401f1-87a4-11e3-834d-670dadc0ddbf",
+      "token": "xjq9h3yzhemf5hfrme8y08fh0sm50zfr",
+      "protocol": "mqtt"
+    });
+
+    conn.on('ready', function(data){
+
+      console.log('Connected to Skynet...');
+
+      conn.on('message', function(channel, data){
+        console.log(data);
+        data = JSON.parse(data);
+        if(data.red == 'on'){
+          console.log("red on request received from skynet");
+          my.led.turnOn();
+
+        } else if(data.red == 'off'){
+          console.log("red off request received from skynet");
+          my.led.turnOff();
+        }     
+      });
+
+    });
+
+
   }
 });
+robot.start();
 
-var skynet = require('skynet');
-var conn = skynet.createConnection({
-  "uuid": "742401f1-87a4-11e3-834d-670dadc0ddbf",
-  "token": "xjq9h3yzhemf5hfrme8y08fh0sm50zfr",
-  "protocol": "mqtt"
-});
-
-conn.on('ready', function(data){
-
-  console.log('Ready');
-
-  conn.on('message', function(channel, data){
-    console.log(data);
-    data = JSON.parse(data);
-    if(data.red == 'on'){
-      console.log("red on request received from skynet");
-      // start working
-      robot.start();
-
-    } else if(data.red == 'off'){
-      console.log("red off request received from skynet");
-      robot.stop();
-
-    }     
-  });
-
-});
