9f5ecb7da5b44339ea425356ff851818013e45fc
Chris Matthieu <chris@matthieu.us>
2014-09-03 15:05:33 -0700

    repeat updates on mqtt

--- npmmqtt.js
+++ npmmqtt.js
@@ -1,10 +1,12 @@
 var skynet = require('skynet-mqtt');
 
 var conn = skynet.createConnection({
-  "uuid": "aa4f9fe1-0ed7-11e4-ba98-ed547cf24cbd",
-  "token": "zlolqmvobl8oajormboq7dvaq8kkpgb9",
-  "qos": 1 // MQTT Quality of Service (0=no confirmation, 1=confirmation, 2=N/A)
-  // "host": "skynet.im", // optional - defaults to skynet.im
+  // "uuid": "aa4f9fe1-0ed7-11e4-ba98-ed547cf24cbd",
+  // "token": "zlolqmvobl8oajormboq7dvaq8kkpgb9",
+  "uuid": "a446d2b0-33f0-11e4-9857-9919b8a9f68e",
+  "token": "05oiih6c08hestt9rbk5f0kxolohto6r",
+  "qos": 1, // MQTT Quality of Service (0=no confirmation, 1=confirmation, 2=N/A)
+  "server": "192.168.100.20", // optional - defaults to skynet.im
   // "port": 1883  // optional - defaults to 1883
 });
 
@@ -27,14 +29,26 @@ conn.on('ready', function(){
   // });
 
 
-  // Broadcast a message to any subscribers to your uuid
-  console.log('sending message', new Date().getTime());
-  conn.message({
-    "devices": "aa4f9fe1-0ed7-11e4-ba98-ed547cf24cbd",
-    "payload": {
-      "hello":"skynet"
-    }
-  });
+  // // Broadcast a message to any subscribers to your uuid
+  // console.log('sending message', new Date().getTime());
+  // conn.message({
+  //   "devices": "aa4f9fe1-0ed7-11e4-ba98-ed547cf24cbd",
+  //   "payload": {
+  //     "hello":"skynet"
+  //   }
+  // });
+
+  setInterval(function(){
+    console.log("sending message");
+    // Send and receive messages
+    conn.message({
+      "devices": "28cdd331-33f1-11e4-9857-9919b8a9f68e",
+      "payload": {
+        "hello":"world"
+      }
+    });
+  },300);
+
 
 
   // // Subscribe to broadcasts from another device
